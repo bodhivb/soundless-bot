eb0dc1ac24789aed63484d25d720b578bccb2183
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2021-01-07 19:52:00 +0100

    Improved index

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-const Discord = require("discord.js");
-const config = require("./config.json");
 const fs = require("fs");
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
 
-const prefix = config.prefix;
 const bot = new Discord.Client({ disableEveryone: true });
 bot.commands = new Discord.Collection();
 
-// Load all commands
+// Loads each command in "commands" folder
 fs.readdir("./commands/", (err, files) => {
   if (err) console.log(err);
 
-  var jsfiles = files.filter((f) => f.split(".").pop() === "js");
-  if (jsfiles.lengte <= 0) {
+  //Only js code may be loaded
+  const jsfiles = files.filter((f) => f.endsWith(".js"));
+  if (jsfiles.length <= 0) {
     return console.log("Couldn't find commands");
   }
 
   jsfiles.forEach((f, i) => {
     delete require.cache[require.resolve(`./commands/${f}`)]; //for reload
-    let cmds = require(`./commands/${f}`);
-    bot.commands.set(cmds.config.name, cmds);
+    const command = require(`./commands/${f}`);
+    bot.commands.set(command.config.name, command);
     console.log(`${f} loaded`);
   });
 });
 
 // Handling an incoming message
 bot.on("message", async (message) => {
-  if (!message.content.startsWith(prefix)) return;
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  let messageArray = message.content.slice(prefix.length + 1).split(" ");
-  let command = messageArray[0].toLowerCase();
-  let args = messageArray.slice(1);
+  const args = message.content.slice(prefix.length + 1).split(" ");
+  const commandName = args.shift().toLowerCase();
 
-  let commandFile = bot.commands.get(command);
-  if (commandFile) commandFile.run(bot, message, args);
+  const command = bot.commands.get(commandName);
+  if (command) command.run(bot, message, args);
 });
 
 bot.on("ready", () => {
-  console.log(`Logged in as '${bot.user.tag}'!`);
+  console.log(`Logged in as '${bot.user.tag}'`);
 });
 
 // Start discord server
-bot.login(config.token);
+bot.login(token);
