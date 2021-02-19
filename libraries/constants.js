9d9ad8bced0dbb6669601c7512e6bc33241a2725
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2021-02-19 22:38:43 +0100

    Improved Minecraft embed

---
+++ libraries/constants.js
@@ -0,0 +1,4 @@
+module.exports.Emojis = Object.freeze({
+  MINECRAFT: "<:minecraft:811723882092167189>",
+  OFFLINE: "🔴",
+});
--- libraries/minecraft.js
+++ libraries/minecraft.js
@@ -1,26 +1,30 @@
 const Discord = require("discord.js");
 const util = require("minecraft-server-util");
+const { Emojis } = require("./constants");
 
 const getMinecraftEmbed = async (mcStatus) => {
-  const embed = new Discord.MessageEmbed().setTitle("Minecraft server");
+  const embed = new Discord.MessageEmbed();
+
+  embed.setTitle("Minecraft server" + Array(70).join(" ") + "\u200B");
   embed.setColor("#008E44");
 
   // Minecraft status need 4 field, so it can take up to 6 different server (24/25 fields)
   for (const mc of mcStatus.server) {
-    const name =
-      "<:minecraft:811723882092167189> " +
-      (mc.name && mc.name !== "" ? mc.name : mc.host);
+    const name = mc.name && mc.name !== "" ? mc.name : mc.host;
 
     try {
       const res = await util.status(mc.host, { port: mc.port });
       const hostname = mc.host + (mc.port === 25565 ? "" : ":" + mc.port);
 
-      embed.addField(name, res.description.toRaw() + "\nIP: `" + hostname + "`");
+      embed.addField(
+        Emojis.MINECRAFT + " " + name,
+        res.description.toRaw() + "\nIP: `" + hostname + "`"
+      );
       embed.addField("Status", "Online", true);
       embed.addField("Players", res.onlinePlayers + "/" + res.maxPlayers, true);
-      embed.addField("Version", res.version + " \n ", true);
+      embed.addField("Version", res.version + "\n\u200B", true);
     } catch {
-      embed.addField(name, "Status\nOffline");
+      embed.addField(Emojis.OFFLINE + " " + name, "Server is offline");
     }
   }
 
