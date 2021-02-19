2a64390c6bda3ffadfc33a2dad4fa5be55b7e18b
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2021-02-19 22:37:33 +0100

    Added mini service

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,8 @@
+const Service = require("../libraries/service");
+
 module.exports = async (bot) => {
   console.log(`Logged in as ${bot.user.tag}`);
   bot.user.setActivity("RuneScape", { type: "PLAYING" });
+
+  new Service(bot);
 };
---
+++ libraries/service.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const { database } = require("./database");
+const { getMinecraftEmbed } = require("./minecraft");
+
+module.exports = class Service {
+  constructor(bot) {
+    this.bot = bot;
+
+    this.refreshMCStatus();
+    setInterval(() => this.refreshMCStatus(), 1 * 60 * 1000);
+  }
+
+  refreshMCStatus() {
+    fs.readdir("./datas/guilds/", (err, files) => {
+      files.forEach(async (file, i) => {
+        if (fs.lstatSync(`./datas/guilds/${file}`).isDirectory()) {
+          const guild = database.getGuild(file);
+          const mcData = guild.getMCStatus();
+
+          if (mcData) {
+            try {
+              const channel = this.bot.channels.cache.get(mcData.channelId);
+              const message = await channel.messages.fetch(mcData.messageId);
+              await message.edit(await getMinecraftEmbed(mcData));
+            } catch (ex) {
+              console.log(ex);
+            }
+          }
+        }
+      });
+    });
+  }
+};
