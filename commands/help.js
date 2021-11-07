dcdb530935de6bcd1c67553d3486b25d8b8b6607
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2021-11-07 17:31:12 +0100

    Added help command

---
+++ commands/help.js
@@ -0,0 +1,32 @@
+const { prefix } = require("../config.json");
+
+module.exports.config = {
+  name: "help",
+  description: "Show you all of the commands.",
+  usage: "help",
+  userPermissions: ["MANAGE_GUILD"],
+};
+
+module.exports.run = async (bot, message, args) => {
+  let helpMessages = [];
+  bot.commands.forEach((command) => {
+    helpMessages.push({
+      name: command.config.name,
+      value:
+        command.config.description +
+        `. \nUsage: \`${prefix}` +
+        command.config.usage +
+        "`",
+    });
+  });
+  message.author.send({
+    embed: {
+      title: "Available commands",
+      description: `[user] = this parameter can be id or user tag.`,
+      color: 12901133,
+      fields: helpMessages,
+    },
+  });
+
+  message.channel.send("Help pages has been sent to you.");
+};
